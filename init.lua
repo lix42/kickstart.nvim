f14db8a487c64ee0a157fe3f4db1375332aadb00
Li Xu <i@xuli.dev>
2025-10-26 15:38:49 -0700

    add comment plugin

--- init.lua
+++ init.lua
@@ -876,6 +876,13 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  {
+    -- Smart and Powerful commenting
+    'numToStr/Comment.nvim',
+    opts = {
+      -- add any options here
+    },
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
